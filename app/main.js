400561e0121ed2c421a2a3825c1a841b0dec8242
David <83370778+novakovicdavid@users.noreply.github.com>
2023-03-25 21:36:23 +0100

    Tint bottom tab labels with theme

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Browse from "./browse";
 import Settings from "./settings";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
-import {ThemeProvider} from "../contexts/themeContext";
+import {ThemeProvider, useThemeContext} from "../contexts/themeContext";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,14 +11,22 @@ export default function Main() {
         <ThemeProvider>
             <SafeAreaProvider>
                 <SafeAreaView style={{flex: 1}}>
-                    <Tab.Navigator screenOptions={{headerShown: false}}>
-                        <Tab.Screen name="Browse" component={Browse}/>
-                        <Tab.Screen name="Profile" component={Browse}/>
-                        <Tab.Screen name="Upload" component={Browse}/>
-                        <Tab.Screen name="Settings" component={Settings}/>
-                    </Tab.Navigator>
+                    <MainView/>
                 </SafeAreaView>
             </SafeAreaProvider>
         </ThemeProvider>
+    )
+}
+
+function MainView() {
+    const {theme} = useThemeContext();
+    console.log(theme);
+    return (
+        <Tab.Navigator screenOptions={{headerShown: false, tabBarActiveTintColor: theme}}>
+            <Tab.Screen name="Browse" component={Browse}/>
+            <Tab.Screen name="Profile" component={Browse}/>
+            <Tab.Screen name="Upload" component={Browse}/>
+            <Tab.Screen name="Settings" component={Settings}/>
+        </Tab.Navigator>
     )
 }
