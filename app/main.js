ef94ea4464ece123cbcb077567325c09183c6c91
David <83370778+novakovicdavid@users.noreply.github.com>
2023-03-25 06:02:51 +0100

    Safe area for tab navigator

--- app/main.js
+++ app/main.js
@@ -1,15 +1,20 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Browse from "./browse";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 
 const Tab = createBottomTabNavigator();
 
 export default function Main() {
     return (
-        <Tab.Navigator screenOptions={{headerShown: false}}>
-            <Tab.Screen name="Browse" component={Browse}/>
-            <Tab.Screen name="Profile" component={Browse}/>
-            <Tab.Screen name="Upload" component={Browse}/>
-            <Tab.Screen name="Settings" component={Browse}/>
-        </Tab.Navigator>
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <Tab.Navigator screenOptions={{headerShown: false}}>
+                    <Tab.Screen name="Browse" component={Browse}/>
+                    <Tab.Screen name="Profile" component={Browse}/>
+                    <Tab.Screen name="Upload" component={Browse}/>
+                    <Tab.Screen name="Settings" component={Browse}/>
+                </Tab.Navigator>
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
