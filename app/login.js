012a7f580ee28e1a78850e31709204cfd8cec9ff
David <83370778+novakovicdavid@users.noreply.github.com>
2023-03-26 03:37:34 +0200

    Login form stylized

--- app/login.js
+++ app/login.js
@@ -1,33 +1,42 @@
-import {View, TextInput, Button, Text, StyleSheet} from "react-native";
-import {useRef} from "react";
+import {View, Button, Text, StyleSheet} from "react-native";
 import {backend} from "../services/backend";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {useRouter} from "expo-router";
 import {storage} from "../services/storage";
+import {useForm} from "react-hook-form";
 
 export default function Login() {
-    const email = useRef();
-    const password = useRef();
     const router = useRouter();
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const onSubmit = data => {
+        backend.login(data.email, data.password)
+            .then((result) => {
+                if (result.profile) {
+                    storage.saveProfile(result.profile);
+                    router.push("/main");
+                }
+            })
+    }
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{flex: 1}}>
                 <View style={styles.titleContainer}>
-                    <Text adjustsFontSizeToFit numberOfLines={1} style={styles.title}>Welcome to Figure.</Text>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={styles.title}>Login to Figure.</Text>
                 </View>
-                <View style={styles.formContainer}>
-                    <TextInput onChangeText={text => email.current = text}/>
-                    <TextInput onChangeText={text => password.current = text}/>
-                    <Button title={"Login"} onPress={() => {
-                        backend.login(email.current, password.current)
-                            .then((result) => {
-                                if (result.profile) {
-                                    storage.saveProfile(result.profile);
-                                    router.push("/main");
-                                }
-                            })
-                    }}/>
-                </View>
+                <form onSubmit={handleSubmit(onSubmit)} style={styles.formContainer}>
+                    <input placeholder={"Email"} {...register("email", {required: true})}
+                           style={styles.formInput}/>
+                    {errors.email && <span style={styles.error}>This field is required</span>}
+
+                    <input placeholder={"Password"} type={"password"} {...register("password", {required: true})}
+                           style={styles.formInput}/>
+                    {errors.password && <span style={styles.error}>This field is required</span>}
+
+                    <View style={styles.submitButton}>
+                        <Button title={"Login"} onPress={handleSubmit(onSubmit)}/>
+                    </View>
+                </form>
             </SafeAreaView>
         </SafeAreaProvider>
     )
@@ -44,12 +53,33 @@ const styles = StyleSheet.create({
     },
     formContainer: {
         flex: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "30em",
+        marginLeft: "5em",
+        marginRight: "5em"
     },
     button: {
         maxWidth: 90,
         width: 90
         // fontSize: 30
+    },
+    formInput: {
+        height: 30,
+        marginTop: 10,
+        borderStyle: "solid",
+        borderColor: "#2196f3",
+        borderWidth: 1,
+        borderRadius: 3
+    },
+    submitButton: {
+        marginTop: 20
+    },
+    error: {
+        fontSize: 13,
+        fontFamily: "-apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Helvetica, Arial, sans-serif",
+        marginTop: 5,
+        marginBottom: 5,
+        color: "red"
     }
 });
